e800968cedff459733f9b3e4be5103bf2c245fdf
AntoineTSIO <antoine.touzart.sio@gmail.com>
2023-11-12 23:23:28 +0100

    fix: update interaction

--- main.py
+++ main.py
@@ -38,9 +38,38 @@ async def ping(interaction: discord.Interaction):
 
 
 @client.tree.command(name="creator", description=f"{os.environ.get('DISCORD_BOT_NAME')} creator")
-async def creator(interraction: discord.Interaction):
-    await interraction.response.send_message(f"This bot was made with :heart: by "
-                                             f"{os.environ.get('DISCORD_BOT_CREATOR')}",
-                                             ephemeral=True)
+async def creator(interaction: discord.Interaction):
+    await interaction.response.send_message(f"This bot was made with :heart: by "
+                                            f"{os.environ.get('DISCORD_BOT_CREATOR')}",
+                                            ephemeral=True)
+
+
+@client.tree.command(name="help", description="Show all available commands")
+async def help(interaction: discord.Interaction):
+    # Create base embed
+    embed = (discord.Embed(
+        title=f"{os.environ.get('DISCORD_BOT_NAME')}",
+        description="Show all currently available commands of this bot"
+    ))
+
+    # Set embed fileds
+    embed.add_field(
+        name="help",
+        value="Show all available commands"
+    )
+    embed.add_field(
+        name="ping",
+        value="Show bot latency"
+    )
+    embed.add_field(
+        name="creator",
+        value="Show bot creator"
+    )
+
+    # Set embed footer
+    embed.set_footer(
+        text=f"Last update: 11/12/2023"
+    )
+    await interaction.response.send_message(embed=embed, ephemeral=True)
 
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
